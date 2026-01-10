3199799d75137cc43c4751c379f7c879aeb34f35
arjun-sthanave <arjun.sthanave@gmail.com>
2026-01-10 17:36:57 +0530

    status added

--- JSON_TASK/dt.js
+++ JSON_TASK/dt.js
@@ -1,6 +1,6 @@
 var nameInput = document.getElementById('name');
 var orderInput = document.getElementById('order');
-
+var StatusBtn = document.getElementById('status')
 var maxInput = document.getElementById('max')
 console.log('max', maxInput);
 
@@ -9,8 +9,11 @@ var SearchBtn = document.getElementById('search')
 
 var nameFilter = ''
 var orderFilter = ''
+var statusFilter = ''
 var minFilter
 var maxFilter
+
+
 const table = new DataTable('#example', {
     ajax: {
         url: './data.json',
@@ -44,13 +47,46 @@ const table = new DataTable('#example', {
                     console.log("filter", filteredData);
 
                 }
+                if (statusFilter === "All") {
+                    return filteredData
+                }
+
+                if (statusFilter) {
+
+
+                    filteredData = filteredData.filter(item =>
+                        item.status
+                            .toLowerCase()
+                            .includes(statusFilter.toLowerCase())
+                    );
+                }
                 return filteredData;
             }
     },
     pageLength: 50,
     lengthChange: false,
     columns: [
+        {
+            className: 'dt-control',
+            orderable: false,
+            data: null,
+            defaultContent: ''
+        },
+        {
+
+
+            render: function (data, type, row, meta) {
+                console.log("data", data);
+                console.log("type", type);
+                console.log("row", row);
+                console.log("meta", meta);
+
+                return meta.settings._iDisplayStart + meta.row + 1;
+            }
+        },
+
         { data: 'customer.firstname' },
+
         { data: 'customer.lastname' },
         { data: 'order_number' },
         { data: 'total' },
@@ -59,7 +95,8 @@ const table = new DataTable('#example', {
         { data: 'source' },
         { data: 'customer.customer_code' },
         { data: 'customer.mobile' }
-    ]
+    ],
+    order: [[1, 'asc']]
 });
 
 
@@ -83,3 +120,40 @@ SearchBtn.addEventListener('click', () => {
     table.ajax.reload();
 
 })
+StatusBtn.addEventListener('click', () => {
+    console.log("status", StatusBtn.value);
+    statusFilter = StatusBtn.value
+    table.ajax.reload();
+})
+
+
+function format(d) {
+    // `d` is the original data object for the row
+    return (
+        '<dl>' +
+        '<dt>Full name:</dt>' +
+        '<dd>' +
+        d.name +
+        '</dd>' +
+        '<dt>Extension number:</dt>' +
+        '<dd>' +
+        d.extn +
+        '</dd>' +
+        '<dt>Extra info:</dt>' +
+        '<dd>And any further details here (images etc)...</dd>' +
+        '</dl>'
+    );
+}
+table.on('click', 'tbody td.dt-control', function (e) {
+    let tr = e.target.closest('tr');
+    let row = table.row(tr);
+
+    if (row.child.isShown()) {
+        // This row is already open - close it
+        row.child.hide();
+    }
+    else {
+        // Open this row
+        row.child(format(row.data())).show();
+    }
+});
